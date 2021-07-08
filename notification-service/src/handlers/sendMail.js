b7dbbe62379c04f62e37a6debe614d7471bbccd0
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-08 20:58:01 +0530

    added sendMail lambda

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,8 +1,31 @@
-async function hello(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+import AWS from 'aws-sdk'
+
+const ses = new AWS.SES({ region: 'eu-west-1' })
+async function sendMail(event, context) {
+  const params = {
+    Source: 'aliarslan1620@gmail.com',
+    Destination: {
+      ToAddresses: ['aliarslan1620@gmail.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from ALI',
+        },
+      },
+      Subject: {
+        Data: 'Test Email',
+      },
+    },
+  }
+
+  try {
+    const result = await ses.sendEmail(params).promise()
+    console.log(result)
+    return result
+  } catch (error) {
+    console.error(error)
   }
 }
 
-export const handler = hello
+export const handler = sendMail
