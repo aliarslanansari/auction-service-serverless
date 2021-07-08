e71d78c69e8d6c50e38dd2b7c1d7a9bc7beb544b
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-08 15:44:09 +0530

    created a util to close the auction

--- auction-service-backend/src/handlers/processAuctions.js
+++ auction-service-backend/src/handlers/processAuctions.js
@@ -1,8 +1,19 @@
+import { closeAuction } from '../lib/closeAuction'
 import { getEndedAuctions } from '../lib/getEndedAuctions'
+import { createErrors } from 'http-errors'
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions()
-  console.log(auctionsToClose)
+  try {
+    const auctionsToClose = await getEndedAuctions()
+    const closePromises = auctionsToClose.map((auction) =>
+      closeAuction(auction.id)
+    )
+    await Promise.all(closePromises)
+    return { closed: closePromises.length }
+  } catch (error) {
+    console.error(error)
+    throw new createErrors.InternalServerError(error)
+  }
 }
 
 export const handler = processAuctions
