f8c8a07cb58f79359c47bb523f27aa4d55235102
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-08 20:12:34 +0530

    added bidder identity and validations

--- auction-service-backend/src/handlers/placeBid.js
+++ auction-service-backend/src/handlers/placeBid.js
@@ -8,11 +8,20 @@ import { getAuctionById } from './getAuction'
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
 let cached = 0
 async function placeBid(event, context) {
+  const { email } = event.requestContext.authorizer
   let updatedAuction
   const { id } = event.pathParameters
   const { amount } = event.body
 
   const auction = await getAuctionById(id)
+
+  if (email === auction.highestBid.bidder) {
+    throw new createErrors.Forbidden(`You are already the highest bidder!`)
+  }
+
+  if (email === auction.seller) {
+    throw new createErrors.Forbidden(`You cannot bid on your own auctions!`)
+  }
 
   if (auction.status === 'CLOSED') {
     throw new createErrors.Forbidden(`You cannot Bid on closed auctions`)
@@ -27,9 +36,11 @@ async function placeBid(event, context) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression:
+      'set highestBid.amount = :amount, highestBid.bidder = :bidder',
     ExpressionAttributeValues: {
       ':amount': amount,
+      ':bidder': email,
     },
     ReturnValues: 'ALL_NEW',
   }
