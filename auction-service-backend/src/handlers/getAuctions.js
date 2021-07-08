7349a534547a1cfad72d64ea6e08205d5202b292
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-08 16:35:03 +0530

    added query param status to fetch auctions by status

--- auction-service-backend/src/handlers/getAuctions.js
+++ auction-service-backend/src/handlers/getAuctions.js
@@ -5,14 +5,26 @@ import commonMiddleware from '../lib/commonMiddleware'
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters
   let auctions
 
+  if (!status) {
+    throw new createErrors.BadRequest("'status' query parameter not provided")
+  }
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  }
+
   try {
-    const results = await dynamoDB
-      .scan({
-        TableName: process.env.AUCTIONS_TABLE_NAME,
-      })
-      .promise()
+    const results = await dynamoDB.query(params).promise()
 
     auctions = results.Items
   } catch (error) {
