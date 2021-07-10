d40f03890e7a179419dc7dd15cc1d4960d6f42f4
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-10 22:12:48 +0530

    added validation to upload picture for auction

--- auction-service-backend/src/handlers/uploadAuctionPicture.js
+++ auction-service-backend/src/handlers/uploadAuctionPicture.js
@@ -3,26 +3,33 @@ import httpErrorHandler from '@middy/http-error-handler'
 import createErrors from 'http-errors'
 import uploadPicturesToS3 from './../lib/uploadPicturesToS3'
 import { getAuctionById } from './getAuction'
+import setAuctionPictureUrl from './../lib/setAuctionPictureUrl'
 
 export async function uploadAuctionPicture(event, context) {
   const { id } = event.pathParameters
+  const { email } = event.requestContext.authorizer
+
   const auction = await getAuctionById(id)
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, '')
   const buffer = Buffer.from(base64, 'base64')
-  try {
-    const uploadToS3Result = await uploadPicturesToS3(
-      `${auction.id}.jpg`,
-      buffer
+
+  if (email !== auction.seller) {
+    throw new createErrors.Forbidden(
+      `You are not authorized to perform this operation!`
     )
-    console.log(uploadToS3Result)
+  }
+  let updatedAuction
+  try {
+    const pictureURL = await uploadPicturesToS3(`${auction.id}.jpg`, buffer)
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureURL)
   } catch (error) {
     console.error(error)
     throw new createErrors.InternalServerError(error)
   }
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   }
 }
 
-export const handler = middy(uploadAuctionPicture).use(httpErrorHandler)
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler())
