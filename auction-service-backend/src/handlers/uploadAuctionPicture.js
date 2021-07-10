b94ec309b9e3ab90852a149a99b6374d9cf4cb4f
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-10 21:00:14 +0530

    upload image to s3 function working

--- auction-service-backend/src/handlers/uploadAuctionPicture.js
+++ auction-service-backend/src/handlers/uploadAuctionPicture.js
@@ -1,4 +1,16 @@
+import { getAuctionById } from './getAuction'
+import uploadPicturesToS3 from './../lib/uploadPicturesToS3'
+
 export async function uploadAuctionPicture(event, context) {
+  const { id } = event.pathParameters
+  const auction = await getAuctionById(id)
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '')
+  const buffer = Buffer.from(base64, 'base64')
+
+  const uploadToS3Result = await uploadPicturesToS3(`${auction.id}.jpg`, buffer)
+
+  console.log(uploadToS3Result)
+
   return {
     statusCode: 200,
     body: JSON.stringify({}),
